4a9a6bbbc6030fd9f8e0ddcf8784ea2a986c8929
Anwar3006 <anwarsadat.d2@gmail.com>
2024-11-01 02:18:21 +0000

    perfect the ui

--- social_multiplication/src/main/resources/static/index.js
+++ social_multiplication/src/main/resources/static/index.js
@@ -50,7 +50,7 @@ function getHistory(alias) {
       data.forEach((i) => {
         $("#tbody").append(`
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white rounded-bl-lg">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
               ${data && i.multiplication.factorA} X 
               ${data && i.multiplication.factorB}
             </th>
